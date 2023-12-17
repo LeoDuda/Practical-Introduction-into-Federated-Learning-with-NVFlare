d1ac262d4bf177734f4265bfc8126de77491095d
LeoDuda <leo.dud97@gmail.com>
2023-12-17 13:00:36 +0100

    restructured the code for extracted CNN and wrapped the whole code inside a main method

--- Exercise08.py
+++ Exercise08.py
@@ -17,6 +17,8 @@ from sklearn.decomposition import PCA
 from sklearn.model_selection import train_test_split
 from torch.utils.data import Dataset, DataLoader
 from torchvision import transforms
+from net import TumorNet
+
 
 device = 'cuda' if torch.cuda.is_available() else 'cpu'
 print(f'Using {device} device')
@@ -26,13 +28,6 @@ do_training = True
 
 img_dir = os.path.realpath('dataset')  # path of image directory
 images = os.listdir(img_dir)
-
-# %% deletable=false editable=false nbgrader={"cell_type": "code", "checksum": "ecaf855fa877962a44e7888d21551aa3", "grade": false, "grade_id": "cell-927bc59378df48fb", "locked": true, "schema_version": 3, "solution": false, "task": false}
-# plot 10 x-ray images
-fig, axes = plt.subplots(2, 5, figsize=(40, 20), gridspec_kw=dict(hspace=0.1, wspace=0.3))
-for i, ax in enumerate(axes.ravel()):
-    ax.imshow(Image.open(os.path.join(img_dir, images[i])))
-    ax.set_title(images[i])
 
 
 # implement tumor_df as pd.DataFrame for train/test split and further data process
@@ -47,216 +42,198 @@ assert len(tumor_df) == 460, 'Please check the set up of tumor_df'
 assert tumor_df["label"].value_counts()[1] == 252
 assert tumor_df["label"].value_counts()[0] == 208
 
-
-# training set/test set split
-train_set, test_set = train_test_split(tumor_df, train_size=0.8, test_size=0.2, random_state=0)
-
-class TumorImageDataset(Dataset):
-    """load, transform and return image and label"""
-
-    def __init__(self, annotations_df, img_dir, transform=None):
-        self.img_labels = annotations_df
-        self.img_dir = img_dir
-        self.transform = transform
-
-    def __len__(self):
-        return len(self.img_labels)
-
-    def __getitem__(self, idx):
-        # get image path according to idx
-        img_path = os.path.join(self.img_dir, self.img_labels.iloc[idx, 0])
-        # convert all image to RGB format
-        image = Image.open(img_path).convert('RGB')
-        label = self.img_labels.iloc[idx, 1]
-        # apply image transform
-        if self.transform:
-            image = self.transform(image)
-        return [image, label]
+def main():
 
 
-# here we resize and cut the center of each image to obtain a dataset with uniform size
-image_transform = transforms.Compose([
-    transforms.Resize(size = (256, 256)),
-    transforms.CenterCrop(size = (244, 244)),
-    transforms.ToTensor()
-])
+    # training set/test set split
+    train_set, test_set = train_test_split(tumor_df, train_size=0.8, test_size=0.2, random_state=0)
 
-# implement Dataset and DataLoader for training
-train_data = TumorImageDataset(train_set, img_dir, image_transform)
-train_dataloader = DataLoader(train_data, batch_size=32, shuffle=False)
+    class TumorImageDataset(Dataset):
+        """load, transform and return image and label"""
 
-test_data = TumorImageDataset(test_set, img_dir, image_transform)
-test_dataloader = DataLoader(test_data, batch_size=32, shuffle=False)
+        def __init__(self, annotations_df, img_dir, transform=None):
+            self.img_labels = annotations_df
+            self.img_dir = img_dir
+            self.transform = transform
 
-image_datasets = {'train': train_data, 'test': test_data}
-image_dataloaders = {'train': train_dataloader, 'test': test_dataloader}
+        def __len__(self):
+            return len(self.img_labels)
 
-feat, labels = next(iter(train_dataloader))
-assert feat.shape[2] == 244 and feat.shape[3] == 244, "Wrong size, please check image_transform"
-
-# plot a batch (32) of image in training set
-train_features, train_labels = next(iter(train_dataloader))  # DataLoader is iterable
-fig, axes = plt.subplots(4, 8, figsize=(40, 20), gridspec_kw=dict(hspace=0.1, wspace=0.3))
-for i, ax in enumerate(axes.flat):
-    ax.imshow(train_features[i].numpy().transpose((1, 2, 0)))
-    ax.set_title('Cancer' if int(train_labels[i] == 1) else 'Not Cancer')
+        def __getitem__(self, idx):
+            # get image path according to idx
+            img_path = os.path.join(self.img_dir, self.img_labels.iloc[idx, 0])
+            # convert all image to RGB format
+            image = Image.open(img_path).convert('RGB')
+            label = self.img_labels.iloc[idx, 1]
+            # apply image transform
+            if self.transform:
+                image = self.transform(image)
+            return [image, label]
 
 
+    # here we resize and cut the center of each image to obtain a dataset with uniform size
+    image_transform = transforms.Compose([
+        transforms.Resize(size = (256, 256)),
+        transforms.CenterCrop(size = (244, 244)),
+        transforms.ToTensor()
+    ])
 
+    # implement Dataset and DataLoader for training
+    train_data = TumorImageDataset(train_set, img_dir, image_transform)
+    train_dataloader = DataLoader(train_data, batch_size=32, shuffle=False)
 
+    test_data = TumorImageDataset(test_set, img_dir, image_transform)
+    test_dataloader = DataLoader(test_data, batch_size=32, shuffle=False)
 
+    image_datasets = {'train': train_data, 'test': test_data}
+    image_dataloaders = {'train': train_dataloader, 'test': test_dataloader}
 
-# Unfortunately, Pytorch does not support the build-in method to display model information (such as the `summary()` method in Tensorflow). Luckily this can be easily implemented. Here is an example:
-def summary(model):
-    """Print out model architecture infomation"""
-    parameter_count = 0
-    model_info = model.state_dict()
-    for name, module in model.named_children():
-        # loop each module in the model to record number of parameters
-        try:
-            n_weight = model_info[name + '.weight'].flatten().shape[0]
-            n_bias = model_info[name + '.bias'].flatten().shape[0]
-        except:
-            n_weight = 0
-            n_bias = 0
-        print(f'{name} layer (No. of weight: {n_weight:n}, No. of bias: {n_bias:n})')
-        parameter_count += (n_weight + n_bias)
-    print(f'Total parameters: {parameter_count:n}')
+    feat, labels = next(iter(train_dataloader))
+    assert feat.shape[2] == 244 and feat.shape[3] == 244, "Wrong size, please check image_transform"
+
+    # plot a batch (32) of image in training set
+    train_features, train_labels = next(iter(train_dataloader))  # DataLoader is iterable
+    fig, axes = plt.subplots(4, 8, figsize=(40, 20), gridspec_kw=dict(hspace=0.1, wspace=0.3))
+    for i, ax in enumerate(axes.flat):
+        ax.imshow(train_features[i].numpy().transpose((1, 2, 0)))
+        ax.set_title('Cancer' if int(train_labels[i] == 1) else 'Not Cancer')
 
 
 
-def train_model(model, loss_func, optimizer, epochs, image_datasets, image_dataloaders, do_training=True):
-    """Return the trained model and train/test accuracy/loss"""
-    if not do_training:
-        return None, None
-    history = {'train_loss': [], 'train_acc': [], 'test_loss': [], 'test_acc': []}
-    for e in range(1, epochs + 1):
-        print('Epoch {}/{}'.format(e, epochs))
-        for phase in ['train', 'test']:
-            if phase == 'train':
-                model.train()  # set model to training mode for training phase
-            else:
-                model.eval()  # set model to evaluation mode for test phase
-
-            running_loss = 0.0  # record the training/test loss for each epoch
-            running_corrects = 0  # record the number of correct predicts by the model for each epoch
-
-            for features, labels in image_dataloaders[phase]:
-                # send data to gpu if possible
-                features = features.to(device)
-                labels = labels.to(device)
-
-                # reset the parameter gradients after each batch to avoid double-counting
-                optimizer.zero_grad()
-
-                # forward pass
-                # set parameters to be trainable only at training phase
-                with torch.set_grad_enabled(phase == 'train'):
-                    outcomes = model(features)
-                    pred_labels = outcomes.round()  # round up forward outcomes to get predicted labels
-                    labels = labels.unsqueeze(1).type(torch.float)
-                    loss = loss_func(outcomes, labels)  # calculate loss
-
-                    # backpropagation only for training phase
-                    if phase == 'train':
-                        loss.backward()
-                        optimizer.step()
-
-                # record loss and correct predicts of each bach
-                running_loss += loss.item() * features.size(0)
-                running_corrects += torch.sum(pred_labels == labels.data)
-
-            # record loss and correct predicts of each epoch and stored in history
-            epoch_loss = running_loss / len(image_datasets[phase])
-            epoch_acc = running_corrects.double() / len(image_datasets[phase])
-
-            print('{} Loss: {:.4f} Acc: {:.4f}'.format(phase, epoch_loss, epoch_acc))
-            history[phase + '_loss'].append(epoch_loss)
-            history[phase + '_acc'].append(epoch_acc)
-
-    return model, history
-
-class TumorNet(nn.Module):
-    """
-    A CNN with:
-        one convolutional layer
-        one max pooling layer
-        two fully connected layers
-    """
-
-    def __init__(self):
-        super(TumorNet, self).__init__()
-        self.conv = nn.Conv2d(in_channels=3, out_channels=16, kernel_size=5, stride=2)
-        self.pool = nn.MaxPool2d(2, 2)
-        self.fc1 = nn.Linear(in_features=60 * 60 * 16, out_features=64)
-        self.dropout = nn.Dropout(p=0.3)
-        self.fc2 = nn.Linear(in_features=64, out_features=1)
-
-    def forward(self, x):
-        """Operations on x"""
-        x = F.relu(self.conv(x))
-        x = self.pool(x)
-        x = torch.flatten(x, 1)
-        x = F.relu(self.fc1(x))
-        x = self.dropout(x)
-        x = torch.sigmoid(self.fc2(x))
-        
-        return x
 
 
-cnn_model = TumorNet().to(device)
-summary(cnn_model)
 
-assert 'conv.weight' in cnn_model.state_dict().keys()
-
-
-cnn_model_trained, cnn_history = train_model(
-    model=cnn_model,
-    loss_func=nn.BCELoss(),
-    optimizer=optim.Adam(cnn_model.parameters(), lr=0.001),
-    epochs=15,
-    image_datasets=image_datasets,
-    image_dataloaders=image_dataloaders,
-    do_training=do_training
-)
-
-# Now we plot the training curve to visualize the loss and accuracy vs. epoch for both training and test process.
-# Note that the hyperparameters of this model are not optimized yet. Feel free to give a try on hyperparameter optimization for better result. But please do it in another notebook.
-
-# %% deletable=false editable=false nbgrader={"cell_type": "code", "checksum": "bb83d934dea56ded02b81f75e4cb2bfb", "grade": false, "grade_id": "cell-411c156c6b081bdf", "locked": true, "schema_version": 3, "solution": false, "task": false}
-def plot_training_curve(history):
-    """Plot the training curve"""
-    train_loss = history['train_loss']
-    test_loss = history['test_loss']
-    train_acc = history['train_acc']
-    test_acc = history['test_acc']
-
-    fig, (ax1, ax2) = plt.subplots(1, 2, figsize=(20, 5))
-    ax1.plot(list(range(1, len(train_loss) + 1)), train_loss, label='Training', color='c')
-    ax1.plot(list(range(1, len(train_loss) + 1)), test_loss, label='Test', color='b')
-    x_major_locator = MultipleLocator(1)
-    ax1.set_xlim(1, len(train_loss))
-    ax1.xaxis.set_major_locator(x_major_locator)
-    ax1.set_xlabel('Eopchs')
-    ax1.set_ylabel('Binary Cross Entropy Loss')
-    ax1.legend(loc='upper right', fontsize='x-large')
-    ax1.set_title('Loss vs. Epochs')
-
-    ax2.plot(np.arange(1, len(train_acc) + 1), train_acc, label='Training', color='c')
-    ax2.plot(np.arange(1, len(train_acc) + 1), test_acc, label='Test', color='b')
-    x_major_locator = MultipleLocator(1)
-    ax2.set_xlim(1, len(train_acc))
-    ax2.xaxis.set_major_locator(x_major_locator)
-    ax2.set_xlabel('Eopchs')
-    ax2.set_ylabel('Accuracy')
-    ax2.legend(loc='lower right', fontsize='x-large')
-    ax2.set_title('Accuracy vs. Epochs')
-
-    plt.show()
-    plt.close()
+    # Unfortunately, Pytorch does not support the build-in method to display model information (such as the `summary()` method in Tensorflow). Luckily this can be easily implemented. Here is an example:
+    def summary(model):
+        """Print out model architecture infomation"""
+        parameter_count = 0
+        model_info = model.state_dict()
+        for name, module in model.named_children():
+            # loop each module in the model to record number of parameters
+            try:
+                n_weight = model_info[name + '.weight'].flatten().shape[0]
+                n_bias = model_info[name + '.bias'].flatten().shape[0]
+            except:
+                n_weight = 0
+                n_bias = 0
+            print(f'{name} layer (No. of weight: {n_weight:n}, No. of bias: {n_bias:n})')
+            parameter_count += (n_weight + n_bias)
+        print(f'Total parameters: {parameter_count:n}')
 
 
-try:
-    plot_training_curve(cnn_history)
-except:
-    pass
+
+    def train_model(model, loss_func, optimizer, epochs, image_datasets, image_dataloaders, do_training=True):
+        """Return the trained model and train/test accuracy/loss"""
+        if not do_training:
+            return None, None
+        history = {'train_loss': [], 'train_acc': [], 'test_loss': [], 'test_acc': []}
+        for e in range(1, epochs + 1):
+            print('Epoch {}/{}'.format(e, epochs))
+            for phase in ['train', 'test']:
+                if phase == 'train':
+                    model.train()  # set model to training mode for training phase
+                else:
+                    model.eval()  # set model to evaluation mode for test phase
+
+                running_loss = 0.0  # record the training/test loss for each epoch
+                running_corrects = 0  # record the number of correct predicts by the model for each epoch
+
+                for features, labels in image_dataloaders[phase]:
+                    # send data to gpu if possible
+                    features = features.to(device)
+                    labels = labels.to(device)
+
+                    # reset the parameter gradients after each batch to avoid double-counting
+                    optimizer.zero_grad()
+
+                    # forward pass
+                    # set parameters to be trainable only at training phase
+                    with torch.set_grad_enabled(phase == 'train'):
+                        outcomes = model(features)
+                        pred_labels = outcomes.round()  # round up forward outcomes to get predicted labels
+                        labels = labels.unsqueeze(1).type(torch.float)
+                        loss = loss_func(outcomes, labels)  # calculate loss
+
+                        # backpropagation only for training phase
+                        if phase == 'train':
+                            loss.backward()
+                            optimizer.step()
+
+                    # record loss and correct predicts of each bach
+                    running_loss += loss.item() * features.size(0)
+                    running_corrects += torch.sum(pred_labels == labels.data)
+
+                # record loss and correct predicts of each epoch and stored in history
+                epoch_loss = running_loss / len(image_datasets[phase])
+                epoch_acc = running_corrects.double() / len(image_datasets[phase])
+
+                print('{} Loss: {:.4f} Acc: {:.4f}'.format(phase, epoch_loss, epoch_acc))
+                history[phase + '_loss'].append(epoch_loss)
+                history[phase + '_acc'].append(epoch_acc)
+
+        return model, history
+
+    net = TumorNet()
+
+
+
+
+    cnn_model = net.to(device)
+    summary(cnn_model)
+
+    assert 'conv.weight' in cnn_model.state_dict().keys()
+
+
+    cnn_model_trained, cnn_history = train_model(
+        model=cnn_model,
+        loss_func=nn.BCELoss(),
+        optimizer=optim.Adam(cnn_model.parameters(), lr=0.001),
+        epochs=15,
+        image_datasets=image_datasets,
+        image_dataloaders=image_dataloaders,
+        do_training=do_training
+    )
+
+    # Now we plot the training curve to visualize the loss and accuracy vs. epoch for both training and test process.
+    # Note that the hyperparameters of this model are not optimized yet. Feel free to give a try on hyperparameter optimization for better result. But please do it in another notebook.
+
+    # %% deletable=false editable=false nbgrader={"cell_type": "code", "checksum": "bb83d934dea56ded02b81f75e4cb2bfb", "grade": false, "grade_id": "cell-411c156c6b081bdf", "locked": true, "schema_version": 3, "solution": false, "task": false}
+    def plot_training_curve(history):
+        """Plot the training curve"""
+        train_loss = history['train_loss']
+        test_loss = history['test_loss']
+        train_acc = history['train_acc']
+        test_acc = history['test_acc']
+
+        fig, (ax1, ax2) = plt.subplots(1, 2, figsize=(20, 5))
+        ax1.plot(list(range(1, len(train_loss) + 1)), train_loss, label='Training', color='c')
+        ax1.plot(list(range(1, len(train_loss) + 1)), test_loss, label='Test', color='b')
+        x_major_locator = MultipleLocator(1)
+        ax1.set_xlim(1, len(train_loss))
+        ax1.xaxis.set_major_locator(x_major_locator)
+        ax1.set_xlabel('Eopchs')
+        ax1.set_ylabel('Binary Cross Entropy Loss')
+        ax1.legend(loc='upper right', fontsize='x-large')
+        ax1.set_title('Loss vs. Epochs')
+
+        ax2.plot(np.arange(1, len(train_acc) + 1), train_acc, label='Training', color='c')
+        ax2.plot(np.arange(1, len(train_acc) + 1), test_acc, label='Test', color='b')
+        x_major_locator = MultipleLocator(1)
+        ax2.set_xlim(1, len(train_acc))
+        ax2.xaxis.set_major_locator(x_major_locator)
+        ax2.set_xlabel('Eopchs')
+        ax2.set_ylabel('Accuracy')
+        ax2.legend(loc='lower right', fontsize='x-large')
+        ax2.set_title('Accuracy vs. Epochs')
+
+        plt.show()
+        plt.close()
+
+
+    try:
+        plot_training_curve(cnn_history)
+    except:
+        pass
+
+if __name__ == "__main__":
+    main()
